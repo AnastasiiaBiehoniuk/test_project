1bc13a818036161d96ccf0bfe93ca24d097d07f0
AnastasiiaBiehoniuk <anastasiiabiehoniuk@gmail.com>
2025-03-29 17:50:26 +0100

    Message 1

--- src/main/java/task_01/Main.java
+++ src/main/java/task_01/Main.java
@@ -2,6 +2,8 @@ package task_01;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test number one");
+
+
+        System.out.println("Test number one v.2");
     }
 }
--- src/main/java/task_01/Test1.java
+++ src/main/java/task_01/Test1.java
@@ -2,6 +2,8 @@ package task_01;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("Test number two");
+
+
+        System.out.println("Test number two v.2");
     }
 }
